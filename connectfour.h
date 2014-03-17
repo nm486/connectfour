b4265e8c88d4be501ce1504a193fb30feebc1eb9
Stanley Tsang <stanleytsang486@gmail.com>
2014-03-17 18:54:32 +0000

    reorganized connect four functions into separate header and source files

---
+++ connectfour.h
@@ -0,0 +1,75 @@
+// Connect Four Header File
+#include <stdlib.h>
+#include <limits.h>
+
+#define BOARD_DIM_X 7
+#define BOARD_DIM_Y 6
+
+static const int PLAYER_ONE = 1;
+static const int PLAYER_TWO = -1;
+static const int EMPTY = 0;
+
+typedef struct point point_type;
+
+struct point
+{
+  int x, y, state;
+};
+
+typedef struct board board_type;
+struct board {
+
+  point_type*** grid;
+  int* heights;
+
+  int cols;
+  int rows;
+
+  int* moves;
+  int lm;
+
+  int cp;
+  point_type*** cl;
+};
+
+point_type* newPoint(int a, int b);
+
+void deletepoint(point_type* p);
+
+int equalsPosition(point_type* a, point_type* b);
+
+void setState(point_type* a, int player);
+
+int getState(point_type* a);
+
+point_type*** generateCL(point_type *** grid);
+
+board_type * createBoard(int a, int b);
+
+void deleteboard(board_type* p);
+
+int validMove(board_type * b, int column);
+
+void makeMove(board_type * b, int column);
+
+void undoMove(board_type * b);
+
+int validMovesLeft(board_type * b);
+
+int getScore(point_type * points[]);
+
+int getStrength(board_type * b);
+
+int winnerIs(board_type * b);
+
+char * toString(board_type * b);
+
+int cp(board_type * b);
+
+int getRandomPlayerMove(board_type *b);
+
+int minValue(board_type * cB, int ply);
+
+int maxValue(board_type * cB, int ply);
+
+int getReasonedMove(board_type * cB);
